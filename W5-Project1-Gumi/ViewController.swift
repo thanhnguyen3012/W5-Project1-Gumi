195a94abfaeb526d5c783191b1d8b9b80aa0286d
Nguyen <nguyennguyen121198@gmail.com>
2021-04-12 09:32:01 +0700

    Initial Commit

---
+++ W5-Project1-Gumi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  W5-Project1-Gumi
+//
+//  Created by Thành Nguyên on 12/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
